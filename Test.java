5878999cb41462c4900a3f6512e535aa226b9fa9
MasunaNaveenKumar <64964628+MasunaNaveenKumar@users.noreply.github.com>
2020-05-08 15:10:58 +0530

    Added my name

--- Test.java
+++ Test.java
@@ -1,14 +1,14 @@
-// Anwesh
+// Java Program to rint yourname.
 class Test
 {
 	public static void main(String[] args)
 	{
-		printName("Anwesh");	
+		printName("Naveen");	
 	}
 
 	public static void printName(String name)
 	{
 		System.out.println("Your name is: "name);
 	}
-
+	
 }
